ed8f45211aaf5d820f24ad0ad4ad3e94c2a6cd40
Robus Gauli <robusgauli@gmail.com>
2021-01-20 15:29:14 +0545

    add a new endpoint

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
+import time
 from locust import HttpUser, task, between
 
-class TestClinicAPI(HttpUser):
+class QuickstartUser(HttpUser):
+    base_url = 'https://svnacovid.crm.dynamics.com/api/data/v9.1'
     host = "https://svnacovid.crm.dynamics.com"
     auth_url  = "https://login.microsoftonline.com/8f4472fe-3110-4c60-b364-ffe3873b1295/oauth2/token"
     wait_time = between(5,10)
-
     access_token = None
 
     @task
-    def hello_world(self):
+    def clinic(self):
         result = self.client.get("/api/data/v9.1", headers=dict(authorization="Bearer {}".format(self.access_token)))
+
+    @task
+    def svna_clinics(self):
+        result = self.client.get("{base_url}/svna_clinicses".format(base_url=self.base_url), headers=dict(authorization="Bearer {}".format(self.access_token)))
+
+    @task
+    def payer_list(self):
+        result = self.client.get("{base_url}/svna_insuranceorganizations".format(base_url=self.base_url), headers=dict(authorization="Bearer {}".format(self.access_token)))
 
     def on_start(self):
         data = dict(grant_type="client_credentials", client_id='6af28d02-3925-4fee-a6b5-97b70cfda92b',  client_secret='HK8BdN_51sM_.dHX2y_tC-Q0Ltvayg6t01', resource='https://svnacovid.crm.dynamics.com/')
