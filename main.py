cb8e1626cb813f8f806bfdc3e0c21769fce58a61
Robus Gauli <robusgauli@gmail.com>
2021-01-20 12:45:09 +0545

    Done with the conf

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ import time
 from locust import HttpUser, task
 
 class QuickstartUser(HttpUser):
+    url  = "http://google.com"
     @task
     def hello_world(self):
-        self.client.get("/hello")
-        self.client.get("/world")
+        self.client.get(url)
+        self.client.get(url)
 
     @task(3)
     def view_item(self):
         for item_id in range(10):
-            self.client.get(f"/item?id={item_id}", name="/item")
+            self.client.get(url)
             time.sleep(1)
 
     def on_start(self):
-        self.client.post("/login", json={"username":"foo", "password":"bar"})
+        self.client.post(url, json={"username":"foo", "password":"bar"})
